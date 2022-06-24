407558bd044d1a6799364d640968e63abd2e9108
Jiayong <jiagains@gmail.com>
2022-06-23 22:49:59 -0400

    Initial working commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to get its sum from 1 to specified number: ");
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input");
+            System.exit(1);
+        }
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = num; i > 0; i--) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
